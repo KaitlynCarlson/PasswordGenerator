1eaa4df3d0c8d9ca2782de0b95a0c96922a7f7fc
Kaitlyn <kaitlyn.carlson7@gmail.com>
2019-12-10 16:51:22 -0800

    index.html

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 var yourPasswordLength = prompt('Your password can be between 8 and 128 characters. What length do you require?');
 
 if (yourPasswordLength >= 8 && yourPasswordLength <= 128) {
-    alert('yourPasswordLength + characters');
+    alert(yourPasswordLength +  ' characters');
 
 }
 else if (yourPasswordLength < 8 || yourPasswordLength > 128) {
@@ -23,6 +23,7 @@ var yourPasswordUpperCaseCharacters = confirm('Would you like upper case charact
 var yourPasswordLowerCaseCharacters = confirm('Would you like lower case characters in your password?');
 
 console.log(yourPasswordLength);
+console.log(yourPasswordNumericCharacters);
 console.log(yourPasswordSpecialCharacters);
 console.log(yourPasswordUpperCaseCharacters);
 console.log(yourPasswordLowerCaseCharacters);
@@ -31,26 +32,29 @@ console.log(yourPasswordLowerCaseCharacters);
 // Variables available to randomly chose from within the user selected parameters
 
 
-var numericCharacters = ["1 2 3 4 5 6 7 8 9"];
-var lowerCaseCharacters = ["qwertyuiopasdfghjklzxcvbnm"];
-var upperCaseCharacters = ["QWERTYUIOPASDFGHJKLZXCVBNM"];
-var specialCharacters = ["! @ # $ % ^ & * _ +"];
-var numericLower = ["1 2 3 4 5 6 7 8 9 qwertyuiopasdfghjklzxcvbnm"];
-var numericLowerUpper = ["1 2 3 4 5 6 7 8 9 qwertyuiQWERTYUIOPASDFGHJKLZXCVBNMopasdfghjklzxcvbnm"];
-var numericLowerUpperSpecial = ["1 2 3 4 5 6 7 8 9 qwertyuiQWERTYUIOPASDFGHJKLZXCVBNMopasdfghjklzxcvbnm! @ # $ % ^ & * _ +"];
-var lowerUpper = ["qwertyuiQWERTYUIOPASDFGHJKLZXCVBNMopasdfghjklzxcvbnm"];
-var lowerUpperSpecial = ["qwertyuiQWERTYUIOPASDFGHJKLZXCVBNMopasdfghjklzxcvbnm! @ # $ % ^ & * _ +"];
-var lowerSpecial = ["! @ # $ % ^ & * _ +qwertyuiopasdfghjklzxcvbnm"];
-var upperSpecial = ["QWERTYUIOPASDFGHJKLZXCVBNM! @ # $ % ^ & * _ +"];
-var numericSpecial = ["1 2 3 4 5 6 7 8 9! @ # $ % ^ & * _ +"];
-var numericUpper= ["1 2 3 4 5 6 7 8 9 QWERTYUIOPASDFGHJKLZXCVBNM"];
-var numericUpperSpecial = ["! @ # $ % ^ & * _ + 1 2 3 4 5 6 7 8 9 QWERTYUIOPASDFGHJKLZXCVBNM"];
+var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseCharacters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var upperCaseCharacters = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var specialCharacters = ["!", "@"," #", "$"," %"," ^", " &"," *", " _", " + "];
 var password = "";
+var generator = [];
 
 
-    for (var i = 0; i = yourPasswordLength; i++) {
-        if ( yourPasswordSpecialCharacters === true && yourPasswordNumericCharacters === false && yourPasswordUpperCaseCharacters===false && yourPasswordLowerCaseCharacters===false) {
-        password = specialCharacters[Math.floor(Math.random()* specialCharacters.length - 1)];
-    } 
-    console.log(password);
+if (yourPasswordSpecialCharacters === true){
+   generator = generator.concat(specialCharacters);
+    console.log(generator);
 }
+
+if (yourPasswordNumericCharacters === true) {
+     generator = generator.concat(numericCharacters);
+    console.log(generator);
+}
+if (yourPasswordUpperCaseCharacters === true) {
+    generator = generator.concat(upperCaseCharacters);
+    console.log(generator);
+}
+if (yourPasswordLowerCaseCharacters === true) {
+    generator = generator.concat(lowerCaseCharacters);
+    console.log(generator);
+}
+
